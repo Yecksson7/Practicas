404677883e026f7f7efea7574be0be8db262134a
Yecksson <Yecksson7Ar@gmail.com>
2024-04-16 20:07:44 -0300

    practica1

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("hola")
+function Prueba ()
+{
+console.log("ejecute la funcion");
+}
+
+Prueba()
+
+console.log("aca estoy")
+
+let funci = () =>{
+    console.log(12345)
+}
+
+window.alert("prueba")
+funci()
+//document.getElementById(id)  Acceder a un elemento html con JS
